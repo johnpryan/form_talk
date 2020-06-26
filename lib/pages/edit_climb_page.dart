da9b6c63c555c0442312eca69cfb201adf01443c
John Ryan <ryjohn@google.com>
2020-06-25 18:23:23 -0700

    Step 7, Ask to discard edits

--- lib/pages/edit_climb_page.dart
+++ lib/pages/edit_climb_page.dart
@@ -29,104 +29,146 @@ class _EditClimbPageState extends State<EditClimbPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          FlatButton(
-            child: Text('Done'),
-            onPressed: () {
-              var valid = _formKey.currentState.validate();
-              if (!valid) {
-                return;
-              }
-              Navigator.of(context).pop(_draft);
-            },
-          ),
-        ],
-      ),
-      body: Form(
-        key: _formKey,
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Name',
-                    hintText: 'Name of the climb...',
-                    filled: true,
+    return WillPopScope(
+      onWillPop: () async {
+        if (_draft == widget.climb) {
+          return true;
+        }
+
+        // Hide the on-screen keyboard permanently.
+        FocusManager.instance.primaryFocus.unfocus();
+
+        // If there are unsaved edits, show a dialog
+        var discard = await showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text('Discard changes?'),
+              actions: [
+                FlatButton(
+                  child: Text(
+                    'Cancel',
+                    style: TextStyle(color: Colors.grey),
                   ),
-                  validator: (s) {
-                    if (s.isEmpty) {
-                      return 'Please enter a name';
-                    }
-                    return null;
-                  },
-                  initialValue: _draft.name,
-                  onChanged: (newValue) {
-                    _draft.name = newValue;
+                  onPressed: () {
+                    Navigator.of(context).pop(false);
                   },
                 ),
-                SizedBox(
-                  height: 24,
-                ),
-                DateFormField(
-                  initialValue: _draft.date,
-                  validator: (dateTime) {
-                    if (dateTime.isAfter(DateTime.now())) {
-                      return 'Please select a date in the past.';
-                    }
-                    return null;
+                FlatButton(
+                  child: Text(
+                    'Discard',
+                    style: TextStyle(color: Colors.red),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop(true);
                   },
-                  onChanged: (newDate) {
-                    _draft.date = newDate;
-                  },
-                ),
-                SizedBox(
-                  height: 24,
-                ),
-                DropdownButtonFormField<GradeType>(
-                  value: _draft.grade.type,
-                  decoration: InputDecoration(filled: true),
-                  onChanged: (gradeType) {
-                    setState(() {
-                      _draft.grade = _draft.grade.toType(gradeType);
-                    });
-                  },
-                  items: [
-                    ...GradeType.values.map(
-                      (type) => DropdownMenuItem<GradeType>(
-                        value: type,
-                        child: Text(describeEnum(type)),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 24,
-                ),
-                DropdownButtonFormField<String>(
-                  value: _draft.grade.toHumanReadableString(),
-                  decoration: InputDecoration(filled: true),
-                  onChanged: (gradeString) {
-                    if (_draft.grade.type == GradeType.FR) {
-                      _draft.grade = Grade.fr(gradeString);
-                    } else {
-                      _draft.grade = Grade.yds(gradeString);
-                    }
-                  },
-                  items: [
-                    ...Grade.listGrades(_draft.grade.type).map(
-                      (gradeString) => DropdownMenuItem<String>(
-                        value: gradeString,
-                        child: Text(gradeString),
-                      ),
-                    ),
-                  ],
-                ),
+                )
               ],
+            );
+          },
+        );
+
+        return discard;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [
+            FlatButton(
+              child: Text('Done'),
+              onPressed: () {
+                var valid = _formKey.currentState.validate();
+                if (!valid) {
+                  return;
+                }
+                Navigator.of(context).pop(_draft);
+              },
+            ),
+          ],
+        ),
+        body: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Name',
+                      hintText: 'Name of the climb...',
+                      filled: true,
+                    ),
+                    validator: (s) {
+                      if (s.isEmpty) {
+                        return 'Please enter a name';
+                      }
+                      return null;
+                    },
+                    initialValue: _draft.name,
+                    onChanged: (newValue) {
+                      _draft.name = newValue;
+                    },
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  DateFormField(
+                    initialValue: _draft.date,
+                    validator: (dateTime) {
+                      if (dateTime.isAfter(DateTime.now())) {
+                        return 'Please select a date in the past.';
+                      }
+                      return null;
+                    },
+                    onChanged: (newDate) {
+                      _draft.date = newDate;
+                    },
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  DropdownButtonFormField<GradeType>(
+                    value: _draft.grade.type,
+                    decoration: InputDecoration(filled: true),
+                    onChanged: (gradeType) {
+                      setState(() {
+                        _draft.grade = _draft.grade.toType(gradeType);
+                      });
+                    },
+                    items: [
+                      ...GradeType.values.map(
+                        (type) => DropdownMenuItem<GradeType>(
+                          value: type,
+                          child: Text(describeEnum(type)),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  DropdownButtonFormField<String>(
+                    value: _draft.grade.toHumanReadableString(),
+                    decoration: InputDecoration(filled: true),
+                    onChanged: (gradeString) {
+                      if (_draft.grade.type == GradeType.FR) {
+                        _draft.grade = Grade.fr(gradeString);
+                      } else {
+                        _draft.grade = Grade.yds(gradeString);
+                      }
+                    },
+                    items: [
+                      ...Grade.listGrades(_draft.grade.type).map(
+                        (gradeString) => DropdownMenuItem<String>(
+                          value: gradeString,
+                          child: Text(gradeString),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
